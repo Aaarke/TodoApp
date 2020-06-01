246862a8fda7dee9df63b725374afeea5d9194b6
Roshan kumar <roshankumarroshi@gmail.com>
2020-06-01 23:58:36 +0530

    counter Ui improvement

--- src/components/counterComponents/Counter.jsx
+++ src/components/counterComponents/Counter.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Counter.css'
 
 
-export default function Counter() {
+class Counter extends Component {
+    constructor(props){
+        super(props)
+        this.state={count:0}
+        this.increament=this.increament.bind(this)
+    }
+    render(){
     return (
       <div className="counterComponent">
-       <button>+1</button>
-       <span className="count">0</span>
+       <button onClick={this.increament}>+1</button>
+    <span className="count">{this.state.count}</span>
       </div>
-    );
+    )}
+
+     increament(){
+        this.setState({count:this.state.count+1})
+    }
   }
+
+  
+
+  export default Counter
